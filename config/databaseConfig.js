cdb967efb6b7a9d3c3d3adfbadd9ffa31377e364
Ibrahem <ibrahem.m.abdalabass@gmail.com>
2023-07-15 14:41:13 +0300

    .

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'user_management',
-  };
+  host: 'bnxcuh7mgxnbydqncn9z-mysql.services.clever-cloud.com',
+  user: 'ub3f7o2mmenlxmre',
+  password: 'QFUzHoJ2NQm52YE1EL0L',
+  database: 'bnxcuh7mgxnbydqncn9z',
+};
