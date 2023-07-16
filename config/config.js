7df2a6fa43363f126ae314887874c36ab8c29836
Ibrahem <ibrahem.m.abdalabass@gmail.com>
2023-07-17 00:02:59 +0300

    config(app): add jwt secret to confi

--- config/config.js
+++ config/config.js
@@ -1,24 +1,28 @@
-require('dotenv').config(); // this is important!
+require("dotenv").config({
+  path: `.env`,
+}); // this is important!
 module.exports = {
-"development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
-},
-"test": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
-},
-"production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
-}
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+  jwtSecret : 'S29s@o@![ld]e23c095^Wx^#@ds}}{11@#'
+
 };
